435b126343a202c0188366cb68fe668594cb0433
keenlight96 <nguyenkimlong29196@gmail.com>
2023-06-30 11:14:44 +0700

    ConnectMySQL

---
+++ src/main/java/com/example/socialnetwork/dao/ConnectMySQL.java
@@ -0,0 +1,20 @@
+package com.example.socialnetwork.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectMySQL {
+    public static final String URL = "jdbc:mysql://localhost:3306/socialnetwork?useSSL=false";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "0123456789";
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
